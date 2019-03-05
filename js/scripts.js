4b5f1722e155799dee184af3a697f538924502e5
Kings Munene <gmaxkings@gmail.com>
2019-03-05 15:43:21 +0300

    indentation..

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//business logic//
+//created by kings munene//
+var current1 = [];
+var current2 = [];
+var total1 = [];
+var total2 = [];
+
+//UI logic//
+$(document).ready(function() {
+  $("#roll1").click(function(){
+    var rollValue = math.floor((Math.random()*6)+1);
+    if (random ==<1){
+      alert("prayer 2 turn");
+    };
+    
+  }
+
+})
