6a09e48b613fb0c6a1d0eda27abd14683bcd5283
Kings Kariuki <47323582+KingsMunene@users.noreply.github.com>
2021-02-14 20:05:08 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 //business logic//
 //created by kings munene//
+var current = [];
 var current1 = [];
-var current2 = [];
+var total = [];
 var total1 = [];
-var total2 = [];
 var sorrymsg = ("sorry you rolled a one, next player please!");
-var sorrymsg2 = ("sorry you rolled a one, next player please!")
+var sorrymsg2 = ("sorry you rolled a one, next player please!");
 
 //UI logic//
+
 //playerone function//
-$(document).ready(function(){
-  $("#roll1").click(function(){
-     event.preventDefault();
-    var current = Math.floor((Math.random()*6)+1);
-    $("#current1").text(current);
+$(document).ready(function()
+ {$("#roll1").click(function()
+  { event.preventDefault();
+    var score = Math.floor((Math.random()*6)+1);
+    $("#current").text(score);
+    var total = add(current);
     if (current == 1){
       $("#roll2").show();
       $("#roll1").hide();
@@ -23,38 +25,31 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
+  $("#hold1").click(function(){
+    event.preventDefault();
+    var total = add(current);
+    $("total1").text(total);
+  });
+});
+
+$(document).ready(function(){
   $("#roll2").click(function(){
      event.preventDefault();
-    var currentb = Math.floor((Math.random()*6)+1);
-    $("#current2").text(currentb);
-    if (currentb == 1){
+    var score1 = Math.floor((Math.random()*6)+1);
+    $("#current1").text(score1);
+    if (score == 1){
       $("#roll1").show();
       $("#roll2").hide();
-   
       $("#sorry2").text(sorrymsg2);
     };
   });
 });
 
 $(document).ready(function(){
-  $("#hold1").click(function(){
-    event.preventDefault();
-    var total = add(current);
-    $("total1").text(total);
-  });
-});
-$(document).ready(function(){
   $("#hold2").click(function(){
     event.preventDefault();
-    var totalb = add(currentb);
-    $("total1").text(totalb);
+    var total1 = add(current1);
+    $("total1").text(total1);
   });
 });
 
-$(document).ready(function(){
-  $("#hold1").click(function(){
-    event.preventDefault();
-    var total = add(current);
-    $("total1").text(total);
-  });
-});
